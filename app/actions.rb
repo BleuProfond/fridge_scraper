328dce7a1c83704c8d8a0b4e83e91d8bede4f7c6
BleuProfond <p.verteuil@gmail.com>
2016-01-28 20:17:19 +0000

    added titles to recipe page

--- app/actions.rb
+++ app/actions.rb
@@ -7,4 +7,12 @@ get '/ingredients' do
   erb :'ingredients'
 end
 
+get '/bookmarks' do
+  erb :'bookmarks'
+end
+
+get '/recipe/:id' do
+  @recipe = Recipe.find(params[:id])
+  erb :'recipe'
+end
 

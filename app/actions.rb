367f84cdbe450b68833fbcb20110f4316d4f3fe0
josephskan <josephskan@live.com>
2016-01-28 18:04:03 -0500

    basic user working

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,18 @@
+helpers do
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+end
+
 # Homepage (Root path)
+
 get '/' do
+  user_id = session[:user_id]
+  if user_id
+    @user = User.find(user_id)
+  else
+    user_id = nil
+  end
   erb :'home_page'
 end
 
@@ -7,13 +20,13 @@ get '/ingredients' do
   erb :'ingredients'
 end
 
-get '/bookmarks' do
-  erb :'bookmarks'
+get '/users/bookmarks' do
+  erb :'/users/bookmarks'
 end
 
 get '/recipe/:id' do
   @recipe = Recipe.find(params[:id])
-  erb :'recipe'
+  erb :'/recipe'
 end
 
 get '/results' do
@@ -26,9 +39,28 @@ get '/results' do
     recipe_ingredients if (recipe_ingredients - @target_ingredients) == []
   end
 
-  erb :'search_result'
+  erb :'/search_result'
 end
 
+get '/user/new' do 
+  @user = User.new
+  erb :'/user/new'
+end
+
+post '/users' do
+  @user = User.create(params)
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    erb :'/users/new'
+  end
+end
+
+get '/users/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
 # post '/results' do
 #   erb :'search_result'
 # end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+
   has_many :recipes
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, uniqueness: true, length: { minimum: 5 }
+  
 end
